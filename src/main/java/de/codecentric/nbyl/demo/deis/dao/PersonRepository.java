32f66563a7b9bd749df14abecbb73e3a00da5c2a
Nicolas Byl <nico@nicolas-byl.eu>
2015-08-31 14:17:00 +0200

    changed to mongodb

--- src/main/java/de/codecentric/nbyl/demo/deis/dao/PersonRepository.java
+++ src/main/java/de/codecentric/nbyl/demo/deis/dao/PersonRepository.java
@@ -1,14 +1,14 @@
 package de.codecentric.nbyl.demo.deis.dao;
 
-import java.util.List;
-
 import de.codecentric.nbyl.demo.deis.model.Person;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+public interface PersonRepository extends MongoRepository<Person, String> {
 
 	List<Person> findByLastName(@Param("name") String name);
 
--- src/main/java/de/codecentric/nbyl/demo/deis/model/Person.java
+++ src/main/java/de/codecentric/nbyl/demo/deis/model/Person.java
@@ -1,33 +1,28 @@
 package de.codecentric.nbyl.demo.deis.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.data.annotation.Id;
 
-@Entity
 public class Person {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+    @Id
+    private String id;
 
-	private String firstName;
-	private String lastName;
+    private String firstName;
+    private String lastName;
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
